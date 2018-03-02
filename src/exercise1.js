e42fd864b99548d65222403764254f3aeddd24a3
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-03-02 10:09:02 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,7 +1,18 @@
 const moment = require("moment");
 
 const sortProduct = input => {
-  // Your future job begins here ...
+  return input
+  .filter(value => moment(value.dateAdded).year() > 2000)
+  .reduce((acc, value) => {
+    var result = {
+      name: value.name,
+      year: moment(value.dateAdded).year(),
+      monthOfYear: moment(value.dateAdded).month(),
+      quantity: value.quantity
+    }
+    acc[value.name] = [result]
+    return acc
+  }, {})
 };
 
 module.exports = {
