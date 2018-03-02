9d701efd85b65d952e742a8b074c54ca275881b6
KevinPautonnier <kevin.pautonnier@ynov.com>
2018-03-02 10:52:50 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,6 +1,25 @@
-const countSequence = (sequence, input) => {
-  // Your future job begins here ...
+const curry2 = (fn) => {
+  console.log(fn.length)
+  return function(sequence){
+    if(sequence.length === 0){
+      return 0
+    }
+    return function(input){
+      if(input.length === 0){
+        return 0
+      }
+      return input.split(sequence).length-1
+    }
+  }
 };
+const toto = (sequence, length) => {
+  if(sequence.length === 0 || input.length === 0){
+    return 0
+  }
+  return input.split(sequence).length-1
+}
+
+const countSequence = curry2(toto)
 
 module.exports = {
   title: "Exercise 4",
